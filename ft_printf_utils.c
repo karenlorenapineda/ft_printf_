7c54ca3e70bee25879eabd7928dffb37062a22a1
Lorena Pineda <94703348+karenlorenapineda@users.noreply.github.com>
2025-02-10 17:46:48 +0100

    Add files via upload

---
+++ ft_printf_utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpineda- <kpineda-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 20:29:56 by kpineda-          #+#    #+#             */
+/*   Updated: 2025/01/13 21:08:31 by kpineda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+int	ft_ptrlen(unsigned long ptr)
+{
+	int	len;
+
+	len = 0;
+	while (ptr > 0)
+	{
+		len++;
+		ptr /= 16;
+	}
+	return (len);
+}
